8918ddd9ad4296b9913b98fbba278f2740c7d90d
dkt64 <b.apanasewicz@gmail.com>
2019-06-24 16:55:06 +0200

    v-model

--- src/components/SingleHero.vue
+++ src/components/SingleHero.vue
@@ -2,20 +2,22 @@
   <div class="single_hero">
     <h1>Edycja bohatera nr {{ $route.params.id }}</h1>
     <h2> {{ $store.state.heroes[$route.params.id].name }} </h2>
-    <input v-model="nazwa">
+    <input v-model="nazwa" v-on:keyup.enter="Zapis">
     <button v-on:click="Zapis">Zapis</button>
   </div>
 </template>
 
 <script>
 
-var nazwa = 'blabla'
-
 export default {
-  data: nazwa,
+  data() {
+    return {
+      nazwa : ''
+    }
+  },
   methods: {
     Zapis: function () {
-      
+      this.$store.state.heroes[this.$route.params.id].name = this.nazwa
     }
   }
 }
@@ -23,8 +25,7 @@ export default {
 </script>
 
 <style>
-ul {
-  text-align: left;
+  ul {
+    text-align: left;
 }
 </style>
-
