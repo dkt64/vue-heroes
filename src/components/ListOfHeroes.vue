d7a25655d2fe95d422cd583404a3bb95d0307d0a
dkt64 <b.apanasewicz@gmail.com>
2019-07-01 22:56:56 +0200

    edit

--- src/components/ListOfHeroes.vue
+++ src/components/ListOfHeroes.vue
@@ -6,9 +6,12 @@
       </div>
       <div class="row">
         <ul class="list-group">
-          <li class="list-group-item" v-for="hero in $store.state.heroes" v-bind:key="hero.id">
-            <h2>{{ hero.name }}</h2>
+          <li class="list-group-item" v-for="hero in this.$heroes" v-bind:key="hero.id">
+            <h2>{{ hero.name }} ID {{ hero.id }}</h2>
             {{hero.desc}}
+            <p>
+            <router-link to="/hero/:id" class="badge badge-primary">Edycja</router-link>
+            </p>
           </li>
         </ul>
       </div>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Router from 'vue-router'
-import Vuex from 'vuex'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -11,7 +10,6 @@ import RouteEditHero from './components/SingleHero'
 Vue.config.productionTip = false
 
 Vue.use(Router)
-Vue.use(Vuex)
 
 var heroes = [
   { id: 1, name: "Herakles", desc: "Znany był z wielkiej siły, waleczności, męstwa, zapaśnictwa i umiejętności wojennych, zwłaszcza celnego strzelania z łuku." },
@@ -21,19 +19,13 @@ var heroes = [
 const routes = [
   { path: '/', component: RouteHeroes },
   { path: '/heroes', component: RouteHeroes },
-  { path: '/hero/:id', component: RouteEditHero },
+  { path: '/hero/:id', component: RouteEditHero}
 ]
 
 const router = new Router ({mode: 'history', routes})
 
-const store = new Vuex.Store({
-  state: {
-    heroes
-  }
-})
-
 new Vue({
   render: h => h(App),
-  router, 
-  store
+  router,
+  heroes
 }).$mount('#app')
